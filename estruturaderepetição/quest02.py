743437557fe12dc3144d59cd97fd029422684b6e
DavidArmando00 <davidarmando0708@gmail.com>
2026-04-16 11:54:00 -0300

    Add files via upload

---
+++ estruturaderepetição/quest02.py
@@ -0,0 +1,8 @@
+total = int(input('digite um numero : '))
+contador = 1
+while contador <= total:
+    print(f' {contador} / {total}')
+    contador = contador + 1 
+
+for contador in range (1, total + 1):
+    print(f' {contador} / {total}')
